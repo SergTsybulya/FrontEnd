e4937cf023db7cf79cd35aa5d3bc608bdcbfc032
SergTsybulya <mac@MacBook-Pro.local>
2020-04-05 10:33:30 +0300

    some changes

--- recursion.js
+++ recursion.js
@@ -23,8 +23,20 @@ function fib(n){
     return (n<=1) ? n : fib(n-1)+fib(n-2);
 }
 
+function pow(x,n){
+    if (n!=1){
+      return x*pow(x, n-1);
+      }else{
+        return x;
+        }
+    }
+    
+console.log(pow(2,3));
+
 console.log(fib(8));
 
 console.log(factorial(5));
 
 console.log(rec());
+
+
