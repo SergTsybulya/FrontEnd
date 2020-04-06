e0570f7691c6ed1cbcbf2f83fee00ec8a89a8938
SergeiTsybulya <sergtsybulya@gmail.com>
2020-04-06 23:25:04 +0300

    add new sorted array

--- array_sorting.js
+++ array_sorting.js
@@ -14,3 +14,21 @@ for (var i = 0; i < m.length; i++){
 }
 
 console.log(m);
+
+
+var arr = [1,3,6,8,9,25,13];
+
+for (var i =0; i<arr.length; i++){
+    var arrMax = arr[i];
+
+    for (var j = i+1; j<arr.length; j++){
+        if (arr[j]<arrMax){
+            var arrNew = arr[j];
+            arrMax = arr[i];
+            arr[j] = arrMax;
+            arr[i] = arrNew;
+        }
+    }
+}
+
+console.log(arr);
